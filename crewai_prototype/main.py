4391965c7014d8f31448e6528fe3879e8d961ebd
Mario <mario.pba@gmail.com>
2024-12-16 18:31:35 +0100

    Update agents configuration and enhance main.py with environment variable loading and structured task management

--- crewai_prototype/main.py
+++ crewai_prototype/main.py
@@ -1,10 +1,75 @@
+# Warning control
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 import yaml
 from crewai import Agent, Task, Crew
 
+os.environ['OPENAI_MODEL_NAME'] = 'gpt-4o-mini'
+
 # Absolute path to the YAML file
 current_directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_directory, "agents.yaml")
+
+# Define file paths for YAML configurations
+files = {
+    'agents': 'crewai_prototype/agents.yaml',
+    'tasks': 'crewai_prototype/tasks.yaml'
+}
+
+# Load configurations from YAML files
+configs = {}
+for config_type, file_path in files.items():
+    with open(file_path, 'r') as file:
+        configs[config_type] = yaml.safe_load(file)
+
+# Assign loaded configurations to specific variables
+agents_config = configs['agents']
+tasks_config = configs['tasks']
+
+# is possible to create a pydanit output structured, like this:
+# this is an example as the agents provide as aoutput a plan for a project based on the
+# input that they ahev received
+
+from typing import List
+from pydantic import BaseModel, Field
+
+class TaskEstimate(BaseModel):
+    task_name: str = Field(..., description="Name of the task")
+    estimated_time_hours: float = Field(..., description="Estimated time to complete the task in hours")
+    required_resources: List[str] = Field(..., description="List of resources required to complete the task")
+
+class Milestone(BaseModel):
+    milestone_name: str = Field(..., description="Name of the milestone")
+    tasks: List[str] = Field(..., description="List of task IDs associated with this milestone")
+
+class ProjectPlan(BaseModel):
+    tasks: List[TaskEstimate] = Field(..., description="List of tasks with their estimates")
+    milestones: List[Milestone] = Field(..., description="List of project milestones")
+
+# Creating Agents
+resource_manager = Agent(
+  config=agents_config['resource_manager']
+)
+
+maintenance_supervisor = Agent(
+  config=agents_config['maintenance_supervisor']
+)
+
+health_monitor = Agent(
+  config=agents_config['health_monitor']
+)
+
+policy_manager = Agent(
+    config=agents_config['policy_manager']
+    )
+
+
 
 # Load agents from a YAML file
 def load_agents_from_yaml(file_path):
